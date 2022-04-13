5c249b9e6480c2dbc022f7c5ba109df556e5a82c
muhreynaldii <reynaldi180101@gmail.com>
2022-04-13 23:36:49 +0700

    add font

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,17 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <div className="min-h-screen bg-gradient-to-b from-gray-600 to-gray-900 font-sans">
+        <nav>
+          <div className="container mx-auto">
+            <div className="flex">
+              <div className="w-2/12">epictetus</div>
+              <div className="w-6/12">Nav</div>
+              <div className="w-4/12">Search</div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </>
   );
 }
