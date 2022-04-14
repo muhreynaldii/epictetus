b0636b1c3d46333caad015772c88b25c059527ee
muhreynaldii <79263343+muhreynaldii@users.noreply.github.com>
2022-04-14 23:28:33 +0700

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,25 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen bg-gradient-to-b from-gray-600 to-gray-900 font-sans">
-        <nav>
+      <div className="min-h-screen bg-gradient-to-b from-gray-600 to-gray-900 font-sans text-white">
+        <nav className="py-10">
           <div className="container mx-auto">
-            <div className="flex">
-              <div className="w-2/12">epictetus</div>
-              <div className="w-6/12">Nav</div>
-              <div className="w-4/12">Search</div>
+            <div className="flex items-center">
+              <div className="w-2/12 flex items-center">
+                <div className="w-10 bg-gray-500 rounded flex items-center justify-center mr-4 shadow-2xl">E</div>
+                epictetus
+              </div>
+              <div className="w-8/12">
+                <ul className="space-x-10 flex items-center">
+                  <li><a href="" className="hover:underline">Ui Design</a></li>
+                  <li><a href="" className="hover:underline">Front-End</a></li>
+                  <li><a href="" className="hover:underline">Back-End</a></li>
+                  <li><a href="" className="hover:underline">Lainnya</a></li>
+                </ul>
+              </div>
+              <div className="w-2/12">
+                <input className="bg-gray-700 py-3 px-6 w-full rounded-full bg-search pl-12" placeholder="Search..."/>
+              </div>
             </div>
           </div>
         </nav>
